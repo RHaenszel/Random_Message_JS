e4dbb7110fb0604be069c3f60a6647f9631ab386
RHaenszel <113142956+RHaenszel@users.noreply.github.com>
2022-09-26 12:22:38 -1000

    Add noun, verb, place array

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 
 
-console.log("Test");
+
+
+const noun = ['person', 'dog', 'lion', 'elephant', 'electrician', 'plumber', 'gardener'];
+const verb = ['walk', 'jump', 'skip', 'run', 'drive', 'swim'];
+const place = ['park', 'city', 'museum', 'art gallery', 'zoo', 'hotel'];
+
+
+
+
+
+
+
+
+
+console.log(`a _____ went for a _____ in the _____`);
