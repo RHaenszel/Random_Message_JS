21352b3d016f2784faa8fe4a0d3e42b2d18fdb46
RHaenszel <113142956+RHaenszel@users.noreply.github.com>
2022-09-26 12:34:09 -1000

    Add random word picker

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ const noun = ['person', 'dog', 'lion', 'elephant', 'electrician', 'plumber', 'ga
 const verb = ['walk', 'jump', 'skip', 'run', 'drive', 'swim'];
 const place = ['park', 'city', 'museum', 'art gallery', 'zoo', 'hotel'];
 
+const randomWord = array => {
+    // console.log(array.length)
+    let word = array[Math.floor(Math.random() * (array.length))];
+    // console.log(word);
+}
 
 
 
 
 
-
-
-
+randomWord(noun);
 console.log(`a _____ went for a _____ in the _____`);
