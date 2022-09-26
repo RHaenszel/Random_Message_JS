58d48b4416ce8b3081ebc7dc90ad8c9d99e3177d
RHaenszel <113142956+RHaenszel@users.noreply.github.com>
2022-09-26 12:51:23 -1000

    Add 3 const to pick words to use in output

--- main.js
+++ main.js
@@ -2,19 +2,22 @@
 
 
 
-const noun = ['person', 'dog', 'lion', 'elephant', 'electrician', 'plumber', 'gardener'];
-const verb = ['walk', 'jump', 'skip', 'run', 'drive', 'swim'];
-const place = ['park', 'city', 'museum', 'art gallery', 'zoo', 'hotel'];
+const nounArray = ['person', 'dog', 'lion', 'elephant', 'electrician', 'plumber', 'gardener'];
+const verbArray = ['walk', 'jump', 'skip', 'run', 'drive', 'swim'];
+const placeArray = ['park', 'city', 'museum', 'art gallery', 'zoo', 'hotel'];
 
 const randomWord = array => {
     // console.log(array.length)
     let word = array[Math.floor(Math.random() * (array.length))];
     // console.log(word);
+    return word;
 }
 
 
 
-
-
-randomWord(noun);
-console.log(`a _____ went for a _____ in the _____`);
+const noun = randomWord(nounArray);
+const verb = randomWord(verbArray);
+const place = randomWord(placeArray); 
+//console.log(noun + verb + place)
+//randomWord(noun);
+console.log(`A _____ went for a _____ in the _____`);
